00f92d8f9f2ccecf6f9a60e1d68ace444cab80b0
Drew Neil <andrew.jr.neil@gmail.com>
2013-05-02 15:16:21 +0100

    Setup basic tabbar with: home, list, form

--- app/view/Main.js
+++ app/view/Main.js
@@ -10,7 +10,7 @@ Ext.define('ThemeDemo.view.Main', {
 
         items: [
             {
-                title: 'Welcome',
+                title: 'Home',
                 iconCls: 'home',
 
                 styleHtmlContent: true,
@@ -29,21 +29,14 @@ Ext.define('ThemeDemo.view.Main', {
                 ].join("")
             },
             {
-                title: 'Get Started',
-                iconCls: 'action',
-
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
-                ]
+                xtype: 'panel',
+                iconCls: 'list',
+                title: 'List'
+            },
+            {
+                xtype: 'panel',
+                iconCls: 'compose',
+                title: 'Form'
             }
         ]
     }
